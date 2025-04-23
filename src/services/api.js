11b200809cff7a67a36743865247ee80a1960d02
temirbayev1997 <abylaihant2@gmail.com>
2025-04-23 14:44:36 +0500

    cors work

--- src/services/api.js
+++ src/services/api.js
@@ -11,7 +11,7 @@ const api = axios.create({
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
-        
+        console.log("Текущий токен:", token);
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
             config.headers["referrerPolicy"] = "unsafe-url";
